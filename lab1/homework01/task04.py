d9c736b9b8b98a8c4e51f042d1cb79df35d4a7bc
Tomasz Szymanski <t_szymanski@onet.eu>
2019-12-17 00:51:11 +0100

    homeworki_zad4

--- lab1/homework01/task04.py
+++ lab1/homework01/task04.py
@@ -5,9 +5,18 @@
 Napisz funkcję sum_from_one_to_n zwracającą sume liczb od 1 do n.
 Jeśli podany argument jest mniejszy od 1 powinna być zwracana wartość 0.
 """
-
+#definicja
 def sum_from_one_to_n(n):
-    pass
+    sum=0
+    if (n>=1):
+        for i in range(n+1):
+            sum+=i
+    else: sum=0
+    return sum
+
 
 input = 999
-output = 499500
+#output = 499500
+
+#wywolanie
+print(sum_from_one_to_n(input))
