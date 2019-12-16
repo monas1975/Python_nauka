10cc8fbcae27d4e18a73075cd53851dedff3486e
Tomasz Szymanski <t_szymanski@onet.eu>
2019-12-16 22:34:32 +0100

    homeworki_zad1

---
+++ lab1/homework01/task01.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+Zad 2. Napisz funkcję even_elements zwracającą listę,
+która zawiera tylko elementy z list o parzystych indeksach.
+"""
+#definicja funkcji
+def even_elements(lista):
+    result=[]
+    for i in range(len(lista)):
+        if i % 2 == 0:
+            result.append(lista[i])
+    return result
+
+
+#input = [1, 2, 3, 4, 5, 6]
+#output = [1, 3, 5]
+
+#wywolanie funkcji
+input = [1, 2, 3, 4, 5, 6]
+output = even_elements(input)
+print(output)
---
+++ lab1/homework01/task02.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+ Napisz funkcję days_in_year zwracającą liczbę dni w roku (365 albo 366).
+"""
+
+# Formalnie sprawdzenie czy dany rok jest rokiem przestępnym możemy zapisać następująco:
+# ((rok mod 4 = 0) and (rok mod 100 <> 0)) or (rok mod 400 = 0)
+
+#Rozwiazanie
+
+def days_in_year(year):
+    pass
+
+input = 2019
+output = 365
