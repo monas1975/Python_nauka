0fdf7ff63aa08b6f41d2021c0e8976ad1b00cb5a
Tomasz Szymanski <t_szymanski@onet.eu>
2019-12-17 01:23:49 +0100

    homeworki_zad5

--- lab1/homework01/task05.py
+++ lab1/homework01/task05.py
@@ -9,8 +9,26 @@ trzyelementowe listy liczb zmiennoprzecinkowych.
 np. odległość pomiędzy punktami (0, 0, 0) i (3, 4, 0) jest równa 5.
 """
 
+#rozwiazanie
+#wzor naodeglosc 2 punktow :
+#d= sqrt([x2-x1]^2 + [y2-y1]^2 + [z2-z1]^2)
+
+
+import math
 def euclidean_distance(x, y):
-    pass
+
+    return math.sqrt((y[0]-x[0])**2+(y[1]-x[1])**2+(y[2]-x[2])**2)
+
 
 input  = [[2.3, 4.3, -7.5], [2.3, 8.5, -7.5]]
-output = 4.2
+input2 = [[0, 0, 0], [3, 4, 0]]
+#output = 4.2
+#wywolanie funkcji
+output = euclidean_distance(input[0],input[1])
+print(output)
+
+output2 = euclidean_distance(input2[0],input2[1])
+print(output2)
+
+
+
