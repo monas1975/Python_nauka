1cc3062e28e69eba548cbdc85e26f515aa2ba9b2
Tomasz Szymanski <t_szymanski@onet.eu>
2019-12-18 00:33:06 +0100

    homeworki_zad10

--- lab1/homework01/task10.py
+++ lab1/homework01/task10.py
@@ -10,9 +10,25 @@ Oba napisy będą składać się wyłacznie z małych liter.
 
 
 def common_chars(string1, string2):
-    pass
+    result=[]
+    string1 = string1.replace(' ', '')  #usuwam spacje
+    l1 = list(string1)                  #zamieniam string na liste
+    l1=list(dict.fromkeys(l1)) #usuwam duplikaty z listy
+    string2 = string2.replace(' ', '')
+    l2 = list(string2)
+    l2 = list(dict.fromkeys(l2))
+
+    for item in l1:
+        print(item)
+        if item in l2:
+            result.append(item)
+    result.sort()
+    return result
 
 
 input1 = "this is a string"
 input2 = "ala ma kota"
 output = ['a', 't']
+
+
+print(common_chars(input1,input2))
